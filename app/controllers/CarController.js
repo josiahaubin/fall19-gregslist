00b19ef9561ff0c2eaf84f0c2ac28dbf9c2b6117
Josiah Aubin <josiahaubin@gmail.com>
2019-08-21 16:58:16 -0600

    Inject car form

--- app/controllers/CarController.js
+++ app/controllers/CarController.js
@@ -63,4 +63,38 @@ export default class CarController {
 
   }
 
+  showCar() {
+    let template = `
+        <div class="form-group">
+        <label for="make">Make</label>
+        <input type="text" class="form-control" name="make" id="make" placeholder="Type in a make" required>
+        </div>
+        <div class="form-group">
+          <label for="model">Model</label>
+          <input type="text" class="form-control" name="model" id="model" placeholder="Type in a model" required>
+        </div>
+        <div class="form-group">
+          <label for="year">Year</label>
+          <input type="number" min="1890" max="2021" class="form-control" name="year" id="year"
+              placeholder="pick a year" required>
+        </div>
+        <div class="form-group">
+          <label for="imgUrl">Image</label>
+          <input type="text" class="form-control" name="imgUrl" id="imgUrl" placeholder="Type in an image address"
+            required>
+        </div>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <input type="text" class="form-control" name="description" id="description"
+            placeholder="Type in a description" required>
+        </div>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" class="form-control" name="price" id="price" placeholder="Type in a price" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Make Car</button>
+    `
+    document.querySelector('#carForm').innerHTML = template
+  }
+
 }
